2a219a60ec6647df4b80352ac92e4435304af62e
feddericahu99 <federica.hu339@gmail.com>
2022-06-23 16:37:03 +0200

    added else to the if

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@
 const tableOfNumbers = document.getElementById('table')
 
 for (let i = 1; i <= 100; i++) { //generatore da 1 a 100
-    console.log(i)
+  
     
     if ( i % 3 ==0 & i % 5 == 0) { //stampa FizzBuzz per i multipli di 3 e 5
         console.log('FizzBuzz');
@@ -16,8 +16,8 @@ for (let i = 1; i <= 100; i++) { //generatore da 1 a 100
         console.log('Buzz');
     }  else if( i % 3 == 0) { //stampa Fizz per i multipli di 3
         console.log('Fizz');
-    } 
+    } else { console.log(i) } //stampo i numeri non multipli di 3 o 5
 
 }
 
-document.getElementById(table).appendChild(i)
+tableOfNumbers.appendChild(i)
