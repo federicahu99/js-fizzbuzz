d071d53f722e6d4ae1e79d760478d9c252302a57
feddericahu99 <federica.hu339@gmail.com>
2022-06-23 15:50:24 +0200

    created generator of numbers in java

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@
 3-Stampare Buzz al posto di 5 e multipli.
 4-Stampare FizzBuzz al posto dei multipli sia di 3 e 5.
 */
+
+for (let i = 1; i <= 100; i++) { //generatore da 1 a 100
+    console.log(i)
+    
+    if ( i % 3 ==0) { //stampa Fizz per i multipli di 3
+        console.log('Fizz')
+    }  
+    if ( i % 5 ==0) { //stampa Buzz per i multipli di 5
+        console.log('Buzz')
+    }  
+    if ( i % 3 ==0 &  i % 5 ==0) { //stampa FizzBuzz per i multipli di 3 e 5
+        console.log('FizzBuzz')
+    }
+}
