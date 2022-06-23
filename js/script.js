4ef76659c50d7fb633c0477e948f2c64c2fed85c
feddericahu99 <federica.hu339@gmail.com>
2022-06-23 15:26:45 +0200

    Created file

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/*
+1-Scrivere un programma che generi numeri da 1 a 100. 
+2-Stampare Fizz al posto di 3 e multipli.
+3-Stampare Buzz al posto di 5 e multipli.
+4-Stampare FizzBuzz al posto dei multipli sia di 3 e 5.
+*/
